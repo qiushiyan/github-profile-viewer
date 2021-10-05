68ef7700b71c1c8be5e283fc792f00c139b657ee
qiushi <qiushi.yann@gmail.com>
2021-10-05 13:00:37 -0500

    update

--- src/components/utils/paginate.js
+++ src/components/utils/paginate.js
@@ -1,5 +1,5 @@
 const paginate = (followers, followersPerPage = 10) => {
-    const followers = followers[0]
+    followers = followers[0]
     const pages = Math.ceil(followers.length / followersPerPage)
     const followersPaginated = Array.from({ length: pages }, (_, index) => {
         const start = index * followersPerPage;
